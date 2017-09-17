2362386eeff02e9ffbc71964a17661e96a4f079d
Jerry Wiltse <jerrywiltse@gmail.com>
2017-09-17 11:33:03 -0400

    Updated version to 1.65.1

--- conanfile.py
+++ conanfile.py
@@ -2,19 +2,19 @@ from conans import ConanFile, tools, os
 
 class BoostLocal_FunctionConan(ConanFile):
     name = "Boost.Local_Function"
-    version = "1.64.0"
+    version = "1.65.1"
     short_paths = True
     url = "https://github.com/bincrafters/conan-boost-local_function"
-    description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
+    description = "Please visit http://www.boost.org/doc/libs/1_65_1/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_names = ["local_function"]
-    requires =  "Boost.Config/1.64.0@bincrafters/testing", \
-                      "Boost.Mpl/1.64.0@bincrafters/testing", \
-                      "Boost.Preprocessor/1.64.0@bincrafters/testing", \
-                      "Boost.Scope_Exit/1.64.0@bincrafters/testing", \
-                      "Boost.Type_Traits/1.64.0@bincrafters/testing",\
-                      "Boost.Typeof/1.64.0@bincrafters/testing",\
-                      "Boost.Utility/1.64.0@bincrafters/testing"
+    requires =  "Boost.Config/1.65.1@bincrafters/testing", \
+                      "Boost.Mpl/1.65.1@bincrafters/testing", \
+                      "Boost.Preprocessor/1.65.1@bincrafters/testing", \
+                      "Boost.Scope_Exit/1.65.1@bincrafters/testing", \
+                      "Boost.Type_Traits/1.65.1@bincrafters/testing",\
+                      "Boost.Typeof/1.65.1@bincrafters/testing",\
+                      "Boost.Utility/1.65.1@bincrafters/testing"
 
                       #config0 mpl5 preprocessor0 scope_exit6 type_traits3 typeof5 utility5
 
