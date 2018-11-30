4f006fbd78b5aae89f49915dc0bda6908db30158
Rene Rivera <grafikrobot@gmail.com>
2018-11-29 22:01:12 -0600

    Initial 1.68.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.67.0@bincrafters/testing")
+base = python_requires("boost_base/1.68.0@bincrafters/testing")
 
 class BoostLocal_FunctionConan(base.BoostBaseConan):
     name = "boost_local_function"
@@ -20,5 +20,3 @@ class BoostLocal_FunctionConan(base.BoostBaseConan):
         "boost_typeof",
         "boost_utility"
     ]
-
-
